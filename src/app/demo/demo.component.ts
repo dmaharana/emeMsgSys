d78a09c88f60cd54c22f70d885d1494d101b3478
Mona Lisa <Mona.Lisa@geemail.com>
2018-01-27 23:43:18 +0530

    used the same modal to edit names

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -8,12 +8,13 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap/modal/modal';
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
     './demo.component.css',
-    '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+    '../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+    '../../../node_modules/font-awesome/css/font-awesome.min.css'
   ]
 })
 export class DemoComponent implements OnInit {
 
-  newNameFormTitle = 'Add a new singer';
+  newNameFormTitle = '';
   name: Name = new Name();
   names: Name[] = [
     {id: 1, fn: 'Kishore', ln: 'Kumar'},
@@ -26,15 +27,50 @@ export class DemoComponent implements OnInit {
 
   addNewName(content): void {
     // console.log('clicked on add');
+    this.newNameFormTitle = 'Add a new singer';
     this.name = {id: 0, fn: '', ln: ''};
     this.modalService.open(content, { windowClass: 'dark-modal' });
   }
 
-  saveNewName(name): void {
+  editName(content, id): void {
+    // console.log('inside editName');
+    this.newNameFormTitle = 'Edit Name';
+    const nameIdx = this.findNameById(id);
+    // console.log(`Name index = ${nameIdx}`);
+    this.name.id = this.names[nameIdx].id;
+    this.name.fn = this.names[nameIdx].fn;
+    this.name.ln = this.names[nameIdx].ln;
+    this.modalService.open(content);
+  }
+
+  saveNewEditName(name): void {
     const lastId = this.names[this.names.length - 1].id;
+    let nameIndex: number;
+    // console.log('inside saveNewEditName');
     // console.log(lastId);
-    const newUser: Name = {id: lastId + 1, fn: name.fn, ln: name.ln};
-    this.names.push(newUser);
+    // console.log(`Id of the returned name ${name.id}`);
+    if (name.id === 0) { // add a new name
+      const newUser: Name = {id: lastId + 1, fn: name.fn, ln: name.ln};
+      this.names.push(newUser);
+    } else { // edit an existing name
+      nameIndex = this.findNameById(name.id);
+      this.names[nameIndex].id = name.id;
+      this.names[nameIndex].fn = name.fn;
+      this.names[nameIndex].ln = name.ln;
+    }
+  }
+
+  private findNameById(id): number {
+    // console.log(`edit name with id=${id}`);
+    // console.log('inside findNameById');
+    let nameIdx = 0;
+    let name: Name = new Name();
+    for (name of this.names) {
+      if (name.id === id) {
+        nameIdx = this.names.indexOf(name);
+      }
+    }
+    return nameIdx;
   }
 
   ngOnInit() {
