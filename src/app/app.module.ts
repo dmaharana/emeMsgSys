636c40cbd1a4b91f6065e1636156907ce9cafc82
Mona Lisa <Mona.Lisa@geemail.com>
2018-01-28 17:22:37 +0530

    sample page loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { SamplePageComponent } from './sample-page/sample-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SamplePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
