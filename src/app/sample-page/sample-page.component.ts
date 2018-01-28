61185167c6cc975f4a2165f6658f31d5366c137b
Mona Lisa <Mona.Lisa@geemail.com>
2018-01-28 17:58:20 +0530

    added modal in a new tab and added css to center it

--- src/app/sample-page/sample-page.component.ts
+++ src/app/sample-page/sample-page.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sample-page',
   templateUrl: './sample-page.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./sample-page.component.css']
 })
 export class SamplePageComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
 
   ngOnInit() {
   }
